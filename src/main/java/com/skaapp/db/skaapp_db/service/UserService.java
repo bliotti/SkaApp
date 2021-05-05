58a7fa4ef4a266571667fa160abee4c02696c4e0
bliotti@protonmail.com <bliotti@protonmail.com>
2021-05-05 03:38:05 +0000

    File generated by Skaffolder - user: bliotti@protonmail.com

---
+++ src/main/java/com/skaapp/db/skaapp_db/service/UserService.java
@@ -0,0 +1,10 @@
+package com.skaapp.db.skaapp_db.service;
+
+import com.skaapp.db.skaapp_db.service.base.UserBaseService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService extends UserBaseService {
+
+	//OVERRIDE HERE YOUR CUSTOM SERVICE
+}
